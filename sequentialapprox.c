da3d5ee77d378715ce727979323701d1011a2834
Seth Schaller <43796711+sas91305@users.noreply.github.com>
2019-11-18 18:37:57 -0500

    Add files via upload
    Sequential Approximation of Pi

---
+++ sequentialapprox.c
@@ -0,0 +1,31 @@
+/*
+*Seth Schaller
+*assn3 - Approximating Pi
+*Sequential Version with for loop
+*10/22/2019
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+	srand(time(0));
+	double x, y, approx, difference, trialnum, in = 0;
+	printf("Enter number of trials: ");
+	scanf(" %lf", &trialnum);
+
+	for (int i = 0; i < trialnum; i++) {
+		x = (2.0 * ((double)rand() / (double)RAND_MAX)) - 1.0; //Random x-coordinate
+		y = (2.0 * ((double)rand() / (double)RAND_MAX)) - 1.0; //Random y-coordinate
+		if (sqrt(pow(x, 2) + pow(y, 2)) <= 1.0) //Distance = sqrt(x^2 + y^2)
+			in++;
+	}
+
+	approx = 4.0 * in / trialnum;
+	difference = fabs(approx - M_PI) / M_PI;
+	printf("Approximization: %lf\n", approx);
+	printf("Difference of approximization: %.7lf\n", difference);
+	return 0;
+}
